e57cf890bfe9a08377638b6796631ccdee745484
ZastenQ <zastenq@gmail.com>
2015-02-01 17:38:19 +0300

    Bacteria in dish/ stable drawing

---
+++ Emulator/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Emulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnDish = new System.Windows.Forms.Panel();
+            this.bnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnDish
+            // 
+            this.pnDish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnDish.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnDish.Location = new System.Drawing.Point(12, 12);
+            this.pnDish.Name = "pnDish";
+            this.pnDish.Size = new System.Drawing.Size(800, 800);
+            this.pnDish.TabIndex = 0;
+            // 
+            // bnRun
+            // 
+            this.bnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnRun.Location = new System.Drawing.Point(293, 829);
+            this.bnRun.Name = "bnRun";
+            this.bnRun.Size = new System.Drawing.Size(244, 95);
+            this.bnRun.TabIndex = 1;
+            this.bnRun.Text = "Run";
+            this.bnRun.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(825, 1182);
+            this.Controls.Add(this.bnRun);
+            this.Controls.Add(this.pnDish);
+            this.Name = "MainForm";
+            this.Text = "Petrie Dish Emulation";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnDish;
+        private System.Windows.Forms.Button bnRun;
+    }
+}
+
---
+++ PetrieDish/Bacterium.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PetrieDish
+{
+    public class Bacterium
+    {
+        public Point CurrentPoint { get; set; }
+        public Double Speed { get; set; }
+        public Point DirectionPoint { get; set; }
+        public Int32 Counter { get; set; }
+        private Random Randomizer { get; set; }
+        private Int32 DirectionCounter { get; set; }
+
+        public Bacterium(Point currPoint, Double speed)
+        {
+            CurrentPoint = currPoint;
+            Speed = speed;
+            Counter = 0;
+            Randomizer = new Random(DateTime.Now.Millisecond);
+            DirectionPoint = new Point(Randomizer.NextDouble(), Randomizer.NextDouble());
+            DirectionCounter = Randomizer.Next(10, 40);
+        }
+
+        public void LifeCicle()
+        {
+            Vector direction = Point.Subtract(CurrentPoint, DirectionPoint);
+            direction = Vector.Multiply(Speed / direction.Length, direction);
+            CurrentPoint = Vector.Add(direction, CurrentPoint);
+            Counter++;
+            DirectionCounter--;
+            if (DirectionCounter == 0)
+            {
+                DirectionPoint = new Point(Randomizer.NextDouble(), Randomizer.NextDouble());
+                DirectionCounter = Randomizer.Next(10, 40);
+            }
+        }
+    }
+}
